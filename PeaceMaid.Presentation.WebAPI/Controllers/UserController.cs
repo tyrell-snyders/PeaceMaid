2d1bc9796f6f56d5d9790293cef612a59e470e6e
tyrell-snyders <tyrell.snyders@gmail.com>
2024-11-04 18:40:11 +0200

    user controller

---
+++ PeaceMaid.Presentation.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeaceMaid.Application.Interfaces;
+using PeaceMaid.Domain.Entities;
+using System.Runtime.InteropServices;
+
+namespace PeaceMaid.Presentation.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUser user) : ControllerBase
+    {
+        private readonly IUser _user = user;
+
+        /// <summary>
+        /// Get All users
+        /// </summary>
+        /// <returns>A List of users</returns>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = await _user.GetAllAsync();
+            return Ok(data);
+        }
+
+        /// <summary>
+        /// Get a specific user from the Database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ServiceResponse</returns>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var result = await _user.GetByIdAsync(id);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Adds a user to the Database
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <returns>ServiceResponse</returns>
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] User userDTO)
+        {
+            if (userDTO == null)
+                return BadRequest("Data of type null cannot be accepted.");
+
+            var result = await _user.AddAsync(userDTO);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Updates a User
+        /// </summary>
+        /// <param name="userDTO"></param>
+        /// <returns>ServiceResponse</returns>
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] User userDTO)
+        {
+            if (userDTO == null)
+                return BadRequest("Data of type null cannot be accepted.");
+
+            var result = await _user.UpdateAsync(userDTO);
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Deletes a User from the Database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ServiceResponse</returns>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _user.DeleteAsync(id);
+            return Ok(result);
+        }
+    }
+}
